0f2896fef8be59cd34c16bf6f37669e62c9eb675
Brunoyam <>
2020-08-21 22:00:35 +0300

    pognali

---
+++ app/script.ts
@@ -0,0 +1,22 @@
+function calc() {
+
+    let ageInputElement:HTMLInputElement = document.querySelector(".input__age") as HTMLInputElement;
+    let age = ageInputElement.value;
+
+    let genderInputElement:HTMLInputElement = document.querySelector("#chooseMen") as HTMLInputElement;
+    let isMan = genderInputElement.checked;
+
+
+    let result = age * sum;
+  
+
+
+
+
+    // function calcAndDisplay() {
+    //     if 
+    // }
+
+    // clickOnSubmit.addEventListener('click', calcAndDisplay);
+
+}
